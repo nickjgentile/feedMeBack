75b43dec577a322aec07ce21bd9aa6f4773ed443
Nick Gentile <nickjgentile@gmail.com>
2018-02-19 13:30:12 -0600

    adding readme.md

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ app.get('/', (req, res) => {
     res.send('Hi There');
 });
 
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Up and running on ${PORT}`)
+});
