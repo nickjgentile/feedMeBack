cb63894e9e0d62e208a3e67ecd429500fa2341eb
Nick Gentile <nickjgentile@gmail.com>
2018-03-01 14:33:11 -0600

    test git integration

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
-
-app.get('/', (req, res) => {
-    res.send('Hi There');
-});
+app.use(express.static(__dirname));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Up and running on ${PORT}`)
+});
+
+app.get('/', (req, res) => {
+    res.send({ bye: 'buddy' });
 });
