361bf7a62cbc55083b524e6a5d0c9cc53fdffa9a
Nick Gentile <nickjgentile@gmail.com>
2018-03-23 11:58:10 -0500

    hiding Google oAuth keys, structuring

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const app = express();
-app.use(express.static(__dirname));
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const app = express();
+passport.use(new GoogleStrategy());
+
+//Port to listen on
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Up and running on ${PORT}`)
-});
-
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
 });
